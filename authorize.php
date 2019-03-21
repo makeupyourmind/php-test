8ef90a0192241486530dcb6a7d8cb8611ec56f79
makeupyourmind <nikitosnov@gmail.com>
2019-03-21 16:10:42 +0200

    ad

--- authorize.php
+++ authorize.php
@@ -6,7 +6,7 @@ $link = pg_connect("host=ec2-23-23-195-205.compute-1.amazonaws.com
  dbname=dbsfrnrnit83ug
  user=rhkqakswlapxxs
  password=7e55e5088ba4df938f3fa4b5d1452e0eb7b5947c5e1f59a41c2038759481276b")
-    or die("Ошибка соединения с базой " . pg_connect($link));
+    or die("connection failed соединения с базой " . pg_connect($link));
   $name = $_POST["name"];
 $query ="SELECT name, password FROM users where name='$name'";
 
