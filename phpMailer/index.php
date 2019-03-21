cbda74679d1168cf513cf32941d01b5b1b7482c0
makeupyourmind <nikitosnov@gmail.com>
2019-03-21 20:13:28 +0200

    ad

--- phpMailer/index.php
+++ phpMailer/index.php
@@ -12,14 +12,14 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = 2;                                       // Enable verbose debug output
+    $mail->SMTPDebug = 1;                                       // Enable verbose debug output
     $mail->isSMTP();                                            // Set mailer to use SMTP
     $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'marinanov040167@gmail.com';                     // SMTP username
     $mail->Password   = 'marinanov';                               // SMTP password
     $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
-    $mail->Port       = 25;                                    // TCP port to connect to
+    $mail->Port       = 587;                                    // TCP port to connect to
 
     //Recipients
     $mail->setFrom('marinanov040167@gmail.com', 'Mailer');
