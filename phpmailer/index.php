9a7f0478d738ed54f18814c76a359be28ca7db7d
makeupyourmind <nikitosnov@gmail.com>
2019-03-21 20:55:23 +0200

    ad

--- phpmailer/index.php
+++ phpmailer/index.php
@@ -1,32 +1,43 @@
 <?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
 require 'vendor/autoload.php';
 
-$mail = new PHPMailer();
-$mail->IsSMTP();
-$mail->CharSet="UTF-8";
-$mail->SMTPSecure = 'tls';
-$mail->Host = 'smtp.gmail.com';
-$mail->Port = 587;
-$mail->Username = 'marinanov040167@gmail.com';
-$mail->Password = 'marinanov';
-$mail->SMTPAuth = true;
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-$mail->From = 'marinanov040167@gmail.com';
-$mail->FromName = 'Mohammad Masoudian';
-$mail->AddAddress('nikitosnov@gmail.com');
-$mail->AddReplyTo('nikitosnov@gmail.com', 'Information');
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                       // Enable verbose debug output
+    $mail->isSMTP();                                            // Set mailer to use SMTP
+    $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'marinanov040167@gmail.com';                     // SMTP username
+    $mail->Password   = 'marinanov';                               // SMTP password
+    $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
 
-$mail->IsHTML(true);
-$mail->Subject    = "PHPMailer Test Subject via Sendmail, basic";
-$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!";
-$mail->Body    = "Hello";
+    //Recipients
+    $mail->setFrom('nikitosnov@gmail.com', 'Mailer');
+    $mail->addAddress('nikitosnov@gmail.com', 'Joe User');     // Add a recipient
+    $mail->addAddress('nikitosnov@gmail.com');               // Name is optional
+    $mail->addReplyTo('nikitosnov@gmail.com', 'Information');
+    $mail->addCC('nikitosnov@gmail.com');
+    $mail->addBCC('nikitosnov@gmail.com');
 
-if(!$mail->Send())
-{
-  echo "Mailer Error: " . $mail->ErrorInfo;
-}
-else
-{
-  echo "Message sent!";
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
 ?>
