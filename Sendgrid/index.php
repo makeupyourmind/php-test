5e93a24e3b1c95b0fcb6e4f88573759970648ee0
makeupyourmind <nikitosnov@gmail.com>
2019-03-21 20:41:03 +0200

    ad

--- Sendgrid/index.php
+++ Sendgrid/index.php
@@ -1,28 +1,36 @@
 <?php
-require 'vendor/autoload.php'; // If you're using Composer (recommended)
-// Comment out the above line if not using Composer
-// require("<PATH TO>/sendgrid-php.php");
-// If not using Composer, uncomment the above line and
-// download sendgrid-php.zip from the latest release here,
-// replacing <PATH TO> with the path to the sendgrid-php.php file,
-// which is included in the download:
-// https://github.com/sendgrid/sendgrid-php/releases
+    // You need to install the sendgrid client library so run:
+    // composer require sendgrid/sendgrid
+    require '/vendor/autoload.php';
 
-$email = new \SendGrid\Mail\Mail();
-$email->setFrom("nikitosnov@gmail.com", "Example User");
-$email->setSubject("Sending with SendGrid is Fun");
-$email->addTo("nikitosnov@gmail.com", "Example User");
-$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-$email->addContent(
-    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-);
-$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
-try {
-    $response = $sendgrid->send($email);
-    print $response->statusCode() . "\n";
-    print_r($response->headers());
-    print $response->body() . "\n";
-} catch (Exception $e) {
-    echo 'Caught exception: '. $e->getMessage() ."\n";
-}
+    // contains a variable called: $API_KEY that is the API Key.
+    // You need this API_KEY created on the Sendgrid website.
+    //include_once('./credentials.php');
+
+    $FROM_EMAIL = 'nikitosnov@gmail.com';
+    // they dont like when it comes from @gmail, prefers business
+    // emails
+
+    $TO_EMAIL = 'nikitosnov@gmail.com';
+    // Try to be nice. Take a look at the anti spam laws. In most
+    // cases, you must have an unsubscribe. You also cannot be
+    // misleading.
+    $subject = "YOUR_SUBJECT";
+    $from = new SendGrid\Email(null, $FROM_EMAIL);
+    $to = new SendGrid\Email(null, $TO_EMAIL);
+    $htmlContent = 'hi';
+    // Create Sendgrid content
+    $content = new SendGrid\Content("text/html",$htmlContent);
+    // Create a mail object
+    $mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+    $sg = new \SendGrid(getenv('SENDGRID_API_KEY'));
+    $response = $sg->client->mail()->send()->post($mail);
+
+    if ($response->statusCode() == 202) {
+     // Successfully sent
+     echo 'done';
+    } else {
+     echo 'false';
+    }
 ?>
