515df7decf6cbb5933a4da3e357c5e1b8414e86e
makeupyourmind <nikitosnov@gmail.com>
2019-03-21 17:09:28 +0200

    ad

--- test.php
+++ test.php
@@ -16,6 +16,7 @@ $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
 $email->addContent(
     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
 );
+echo "ok";
 $sendgrid = new \SendGrid(getenv($_ENV['SENDGRID_API_KEY']));
 try {
     $response = $sendgrid->send($email);
