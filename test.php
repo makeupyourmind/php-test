0721e6c4098f2e2ccb13b5bf379f48e9f762726d
makeupyourmind <nikitosnov@gmail.com>
2019-03-21 17:51:47 +0200

    ad

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 	// You need to install the sendgrid client library so run: composer require sendgrid/sendgrid
-	require './sendgrid-php/vendor/autoload.php';
+	require '/vendor/autoload.php';
 	// contains a variable called: $API_KEY that is the API Key.
 	// You need this API_KEY created on the Sendgrid website.
 	include_once('credentials.php');
