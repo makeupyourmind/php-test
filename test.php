8dbb3ff4bd33db0731b731c13b4690dd1ae6cd33
makeupyourmind <nikitosnov@gmail.com>
2019-03-21 17:32:39 +0200

    ad

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 	// You need to install the sendgrid client library so run: composer require sendgrid/sendgrid
-	require './sendgrid-php/vendor/autoload.php';
+	require '/sendgrid-php/vendor/autoload.php';
 	// contains a variable called: $API_KEY that is the API Key.
 	// You need this API_KEY created on the Sendgrid website.
 	include_once('credentials.php');
@@ -11,13 +11,13 @@
 	// Try to be nice. Take a look at the anti spam laws. In most cases, you must
 	// have an unsubscribe. You also cannot be misleading.
 	$subject = "YOUR_SUBJECT";
-	/*$from = new SendGrid\Email(null, $FROM_EMAIL);
+	$from = new SendGrid\Email(null, $FROM_EMAIL);
 	$to = new SendGrid\Email(null, $TO_EMAIL);
 	$htmlContent = 'HI';
 	// Create Sendgrid content
 	$content = new SendGrid\Content("text/html",$htmlContent);
 	// Create a mail object
-	$mail = new SendGrid\Mail($from, $subject, $to, $content);*/
+	$mail = new SendGrid\Mail($from, $subject, $to, $content);
 
   echo "hi";
 	/*$sg = new \SendGrid($API_KEY);
