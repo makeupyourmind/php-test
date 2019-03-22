2ccf7b562df9c792d16e19ae1a4e7a2ffb78bc76
makeupyourmind <nikitosnov@gmail.com>
2019-03-22 15:47:30 +0200

    ad

--- phpmailer/index.php
+++ phpmailer/index.php
@@ -57,6 +57,11 @@ try {
     $mail->addCC('nikitosnov@gmail.com');
     $mail->addBCC('nikitosnov@gmail.com');
 
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
     $mail->send();
     echo 'Message has been sent';
 } catch (Exception $e) {
