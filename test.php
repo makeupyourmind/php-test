6e80d871c7248b0843a444ff2ff564da7cc62814
makeupyourmind <nikitosnov@gmail.com>
2019-03-22 23:18:25 +0200

    ad

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-  echo "host : ".getenv("HOST");
-  echo "<br>";
-  echo "user : ".getenv("USER");
-  echo "<br>";
-  echo "password : ".getenv("PASSWORD");
-  echo "<br>";
-  echo "port : ".getenv("PORT");
- ?>
