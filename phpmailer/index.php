04cc8fb318858791aec26345a750e58fbd2ff84f
makeupyourmind <nikitosnov@gmail.com>
2019-03-22 15:52:02 +0200

    ad

--- phpmailer/index.php
+++ phpmailer/index.php
@@ -44,9 +44,9 @@ try {
                                                // 1 = errors and messages
                                                // 2 = messages only
     $mail->SMTPAuth   = true;                  // enable SMTP authentication
-    $mail->SMTPSecure = "ssl";                 // sets the prefix to the servier
+    $mail->SMTPSecure = "tsl";                 // sets the prefix to the servier
     $mail->Host       = "smtp.gmail.com";      // sets GMAIL as the SMTP server
-    $mail->Port       = 465;                   // set the SMTP port for the GMAIL server
+    $mail->Port       = 587;                   // set the SMTP port for the GMAIL server
     $mail->Username   = "marinanov040167@gmail.com";  // GMAIL username
     $mail->Password   = "marinanov";
 
