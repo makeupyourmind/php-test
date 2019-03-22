d3c76c7ff51c329b82a178806f3877c5478d1a4d
makeupyourmind <nikitosnov@gmail.com>
2019-03-22 18:03:14 +0200

    ad

---
+++ SendGrid-API/index.php
@@ -0,0 +1,19 @@
+<?php
+/*SendGrid Library*/
+require_once ('vendor/autoload.php');
+
+/*Content*/
+$from = new SendGrid\Email("nikitosnov@gmail.com", "nikitosnov@gmail.com");
+$subject = "SUBJECT";
+$to = new SendGrid\Email("nikitosnov@gmail.com", "nikitosnov@gmail.com");
+$content = new SendGrid\Content("text/html", "CONTENT_GOES_HERE");
+
+/*Send the mail*/
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
+
+/*Response*/
+$response = $sg->client->mail()->send()->post($mail);
+print $response->statusCode() . "\n";
+?>
